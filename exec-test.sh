804470738073d2a44613c73d416f02d3442d7e91
Jason Hsu <jhsu@midwestmortgageanalytics.com>
2020-05-11 16:16:57 -0500

    test.sh successfully creates new Gatsby app ready for modification

--- exec-test.sh
+++ exec-test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-DATE=`date +%Y%m%d-%H%M%S-%3N`
 APP_NAME='tmp1'
-DIR_APP=$PWD/new_apps/$APP_NAME
+DIR_APP=$PWD/$APP_NAME
 
 mkdir -p log
 bash exec-main.sh $APP_NAME 'no_outline'
--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 DATE=`date +%Y%m%d-%H%M%S-%3N`
 APP_NAME='tmp1'
-DIR_APP=$PWD/$APP_NAME
 
 mkdir -p log
 bash exec-test.sh $APP_NAME 2>&1 | tee log/test-$DATE.txt
