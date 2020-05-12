5a45b434da9717ad8f4c0c9be8c8e9a09bc47bc9
Jason Hsu <jhsu@midwestmortgageanalytics.com>
2020-05-12 00:37:22 -0500

    Makes it through Chapter 1; need to revise Chapter 2

--- mod/mod-01/mod-01.sh
+++ mod/mod-01/mod-01.sh
@@ -7,13 +7,9 @@ echo '#########################'
 
 DIR_MAIN=$PWD
 
-echo '----------------------------------'
-echo 'Removing yarn.lock from .gitignore'
-sed -i -n "/.yarn.lock/{p; :a; N; //!ba; s/.*\n//}; p" .gitignore
-
-echo '------------------------------------'
-echo "cd $DIR_MAIN/website && yarn install"
-cd $DIR_MAIN/website && yarn install
+echo '---------------------------'
+echo "cd $DIR_MAIN && npm install"
+cd $DIR_MAIN && npm install
 
 cd $DIR_MAIN && git init
 cd $DIR_MAIN && git add .
