983527fa8a7f2d0d074d41b40341ab5e6731bc1e
Jason Hsu <jhsu@midwestmortgageanalytics.com>
2020-05-21 17:07:15 -0500

    Removed redundant message

--- exec-main.sh
+++ exec-main.sh
@@ -4,12 +4,3 @@ set -e
 APP_NAME=$1
 
 docker run -i -t --rm -v ${PWD}:/home/node/neutrino rubyonracetracks/node_user /home/node/neutrino/build-gatsby $APP_NAME
-
-echo '##########################################'
-echo 'The new app has been created from scratch!'
-echo ''
-echo "It is located at: $DIR_APP"
-echo ''
-echo 'Things to check:'
-echo '* When you run the build.sh script, there should be no errors or failures.'
-echo ''
